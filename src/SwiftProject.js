f0103f52e9884dcdcae75b321a91c4689a0cb3cc
Troy Lemons Jr <troylemonsjr@gmail.com>
2022-12-10 11:58:15 -0800

    added videos

--- src/SwiftProject.js
+++ src/SwiftProject.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const SwiftProject = () => {
     return (
-        <div className="row1">
+        <div className="intro">
             <h1>Troy Lemons Jr.</h1>
             <h3>Hi my name is Troy, I'm a growing Software Developer,<br/> 
                 I have been working on sharpening my skills the last <br/>
@@ -11,8 +11,13 @@ const SwiftProject = () => {
                 Ruby, and PHP along with a view frameworks. Today <br/>
                 I'm here to show you a couple of swift projects that I <br/>
                 got to work on using SwiftiOS.
-                </h3>
-
+                </h3> <br/>
+            <video width="750" height="500" controls> 
+            <source src="./Videos/humbleLemon.mp4" type="video/mp4"/>
+            </video>
+            <video width="750" height="500" controls> 
+            <source src="./Videos/memoryGame.mp4" type="video/mp4"/>
+            </video>
         </div>
     )
 }
